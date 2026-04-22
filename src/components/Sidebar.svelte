cf2bebab8e27625e3fd67b2f1bc67d035d75ef51
DJsexum <juancruzcabrerasocin@gmail.com>
2026-04-21 23:10:50 -0300

    No compila

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { ROUTERS } from "@utils/router";
+
+    const { activePage } = $props();
+</script>
+
+<aside>
+    {#each ROUTERS as route}
+        <p class="text-white"> {route.name}</p>
+    {/each}
+</aside>
---
+++ src/core/utils/router.ts
@@ -0,0 +1,15 @@
+export const ROUTERS = 
+{
+    DASHBOARD: 
+    { 
+        path: "/",
+        name: "Dashboard",
+        private: true,
+    },
+    LOGIN:
+    {
+        path: "/auth/login",
+        name: "Iniciar Sesion",
+        public: true,
+    },
+}
