a89da35128dae9f888428edbe5693191a6fce893
DJsexum <juancruzcabrerasocin@gmail.com>
2026-05-04 23:13:19 -0300

    agregamos boton de modo oscuro

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 
     import { ROUTERS } from "@utils/router";
+    import { appStore } from "@utils/store.svelte";
+
     let { activePage } = $props();
 
 </script>
@@ -18,5 +20,15 @@
         <a href={route.path} class="text-black text-md py-1 px-2 bg-gray-300/50 rounded-md"> {route.name} </a>
         
     {/each}
+
+    <dvi clas="p-2 flex justify center">
+
+        <button onclick={() => appStore.toggleDarkMode()} class="bg-gray-800 text-white px-4 py-2 rounded-md">
+
+            {appStore.isDarkMode ? '☀️' : '🌙'}
+
+        </button>
+
+    </dvi>
 
 </aside>
