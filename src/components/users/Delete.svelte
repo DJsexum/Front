de8da1a02111e339d118175fa2476dc9757fa162
DJsexum <juancruzcabrerasocin@gmail.com>
2026-05-12 22:54:45 -0300

    Correccion de botones y creacion de cositas

---
+++ src/components/users/Delete.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+    let { userModel } = $props();
+
+</script>
+
+{#if userModel.deleteDialog}
+
+    <dialog open={userModel.deleteDialog} class="w-full h-full fixed top-0 left-0 flex items-center bg-transparent justify-center backdrop-blur-xl">
+
+        <div class="bg-white p-4 rounded-md w-96">
+
+            <button aria-label="Cerrar" class="close" onclick={() => (userModel.deleteDialog = false)}>
+                
+                ❌
+            
+            </button>
+
+            <div class="p-4">
+
+                <h2 class="text-lg font-bold">
+
+                    Desea eliminar al usuario {userModel.user.username}?
+
+                </h2>
+
+            </div>
+
+        </div>
+
+    </dialog>
+
+{/if}
--- src/components/users/Tabla.svelte
+++ src/components/users/Tabla.svelte
@@ -1,53 +1,61 @@
 <script lang="ts">
 
-    import {onMount} from "svelte";
+    import Delete from "./Delete.svelte";
+    import Edit from "./Edit.svelte";
+    import { onMount } from "svelte";
+    import { userModel } from "./user.svelte";
 
-    let users: any = $state([]);
-
-    onMount
-    (async () =>
-        {
-            users = await fetch('https://dummyjson.com/users'). then(res => res.json())
-        }
-    )
+    onMount (async () =>
+    {
+        await userModel.getUsers();
+    });
 
 </script>
+
+
+<Delete {userModel}/>
+<Edit {userModel}/>
+
 
 <table class="flex-1 w-full">
 
     <thead>
 
         <tr>
-            
-            <th class="bg-gray-800 text-white text-left px-2">Nombre</th>
-            <th class="bg-gray-800 text-white text-left px-2">Email</th>
-            <th class="bg-gray-800 text-white text-left px-2">Acciones</th>
-            
+
+            <th class="bg-gray-800 text-white text-left p-2">Nombre</th>
+            <th class="bg-gray-800 text-white text-left p-2">Email</th>
+            <th class="bg-gray-800 text-white p-2">Acciones</th>
+
         </tr>
 
     </thead>
 
     <tbody>
 
-        {#each users.users as user}
+        {#each userModel.users as user}
 
             <tr class="odd:bg-gray-100 dark:odd:bg-gray-700">
-                
-                <td  class="px-2 py-1"> {user.username}</td>
-                <td class="px-2 py-1"> {user.email}</td>
+
+                <td class="px-2 py-1">{user.username}</td>
+                <td class="px-2 py-1">{user.email}</td>
                 <td class="px-2 py-1">
 
-                    <buton aria-label="editar" class="bg-gray-800 text-white px-4 py-2 rounded-md">
+                    <div class="flex justify-center gap-2">
 
-                        ✏️
-                         
-                    </buton>
+                        <button onclick={() => userModel.showEditModal(user)} aria-label="Editar" class="bg-gray-800 text-white px-4 rounded-md">
 
-                    <buton aria-label="eliminar" class="bg-red-500 text-white px-4 py-2 rounded-md">
-                        
-                        🗑️
+                            ✏️
 
-                    </buton>
+                        </button>
+
+                        <button onclick={() => userModel.showDeleteModal(user)} aria-label="Eliminar" class="bg-red-500 text-white px-4 rounded-md">
+
+                            🗑️
+
+                        </button>
+
+                    </div>
 
                 </td>
 
